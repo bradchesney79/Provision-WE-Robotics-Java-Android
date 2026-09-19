54282272f58919e08372306efdf1469f28ef2bcf
Bradley <bradchesney79@gmail.com>
2026-09-19 17:34:59 -0400

    added a chrome config to restore previous session tabs

--- chrome.sh
+++ chrome.sh
@@ -32,3 +32,7 @@ echo '{
     }
   }
 }' | sudo tee /etc/opt/chrome/policies/managed/extension_pin.json
+
+echo '{
+  "RestoreOnStartup": 1
+}' | sudo tee /etc/opt/chrome/policies/managed/restore_tabs.json
